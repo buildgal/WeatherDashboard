32b053597b19dcd583abcc632e84548598d51324
buildgal <gjaquelin7@outlook.com>
2020-11-08 16:09:49 -0600

    adding the framework for the index

---
+++ script.js
@@ -0,0 +1,19 @@
+let searchBtn= $("#searchBtn");
+
+searchBtn.on("click", function() {
+  
+
+
+ });
+
+
+//search for a city 
+
+//call the API 
+
+//pull out the data from the API 
+
+//display the icon based off the weather 
+//if rain then show clouds 
+
+//show the index 
