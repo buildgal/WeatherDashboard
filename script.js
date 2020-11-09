f8b3d2cc6b74e39911bfacec1e4395b8f8d09547
buildgal <gjaquelin7@outlook.com>
2020-11-09 01:21:55 -0600

    adding the icons to the weather dashboard

--- script.js
+++ script.js
@@ -66,6 +66,8 @@ searchBtn.on("click", function(event) {
     let forcastURL="http://api.openweathermap.org/data/2.5/forecast?q="+
     city+"&appid="+
     apiKey+"&units=standard";
+    iconUrl="http://openweathermap.org/img/wn/";
+
 
     $.ajax({
         url: forcastURL,
@@ -77,95 +79,108 @@ searchBtn.on("click", function(event) {
 
         //Day 1 
         elDay1=$("#date1");
-        dateDay1=moment().add(1, 'days').calendar();
+        let dateDay1=moment().add(1, 'days').calendar();
         elDay1.append(dateDay1);
-        //parse out the time "at"
-
-        //Day 1 Temp 
-        elTemp1=$("#temp1");
-        dataTemp1=response.list[0].main.temp;
-        day1Temp=Math.floor((dataTemp1-273.15)*1.8+32);
-        elTemp1.append(day1Temp);
-
-        //Day 1 Humidity
-        elHum1=$("#hum1");
-        dataHum1=response.list[0].main.humidity;
-        elHum1.append(dataHum1);
+            //Day 1 Temp 
+            elTemp1=$("#temp1");
+            dataTemp1=response.list[0].main.temp;
+            let day1Temp=Math.floor((dataTemp1-273.15)*1.8+32);
+            elTemp1.append(day1Temp);
+                //Day 1 Humidity
+                elHum1=$("#hum1");
+                dataHum1=response.list[0].main.humidity;
+                elHum1.append(dataHum1);
+                    //Day 1 Icon
+                    let elIcon1=$("#icon1");
+                    let dataIcon1=response.list[0].weather[0].icon;
+                    let icon1Img=iconUrl+dataIcon1+"@2x.png";
+                    elIcon1.attr("src", icon1Img);
+                    
+                   
 
         
         //Day 2 
         elDay2=$("#date2");
         dateDay2=moment().add(2, 'days').calendar();
         elDay2.append(dateDay2);
-
-        //Day 2 Temp 
-        elTemp2=$("#temp2");
-        dataTemp2=response.list[1].main.temp;
-        dayTemp2=Math.floor((dataTemp2-273.15)*1.8+32);
-        elTemp2.append(dayTemp2);
-
-        //Day 2 Hum
-        elHum2=$("#hum2");
-        dataHum2=response.list[1].main.humidity;
-        elHum2.append(dataHum2);      
-
+            //Day 2 Temp 
+            elTemp2=$("#temp2");
+            dataTemp2=response.list[1].main.temp;
+            dayTemp2=Math.floor((dataTemp2-273.15)*1.8+32);
+            elTemp2.append(dayTemp2);
+                //Day 2 Hum
+                elHum2=$("#hum2");
+                dataHum2=response.list[1].main.humidity;
+                elHum2.append(dataHum2);
+                    //Day 2 Icon
+                    let elIcon2=$("#icon2");
+                    let dataIcon2=response.list[1].weather[0].icon;
+                    let icon2Img=iconUrl+dataIcon2+"@2x.png";
+                    elIcon2.attr("src", icon2Img);
+                    
+                    
         //Day 3      
         elDay3=$("#date3");
         dateDay3=moment().add(3, 'days').calendar();
         elDay3.append(dateDay3);
-
-        //Day 3 Temp 
-        elTemp3=$("#temp3");
-        dataTemp3=response.list[2].main.temp;
-        dayTemp3=Math.floor((dataTemp3-273.15)*1.8+32);
-        elTemp3.append(dayTemp3);
-
-        //Day 3 Hum
-        elHum3=$("#hum3");
-        dataHum3=response.list[2].main.humidity;
-        elHum3.append(dataHum3);     
+            //Day 3 Temp 
+            elTemp3=$("#temp3");
+            dataTemp3=response.list[2].main.temp;
+            dayTemp3=Math.floor((dataTemp3-273.15)*1.8+32);
+            elTemp3.append(dayTemp3);
+                //Day 3 Hum
+                elHum3=$("#hum3");
+                dataHum3=response.list[2].main.humidity;
+                elHum3.append(dataHum3); 
+                    //Day 3 Icon
+                    let elIcon3=$("#icon3");
+                    let dataIcon3=response.list[2].weather[0].icon;
+                    let icon3Img=iconUrl+dataIcon3+"@2x.png";
+                    elIcon3.attr("src", icon3Img);
+                       
 
         //Day 4 
         elDay4=$("#date4");
         dateDay4=moment().add(4, 'days').calendar();
         elDay4.append(dateDay4);
-
-        //Day 4 Temp 
-        elTemp4=$("#temp4");
-        dataTemp4=response.list[3].main.temp;
-        dayTemp4=Math.floor((dataTemp3-273.15)*1.8+32);
-        elTemp4.append(dayTemp4);
-
-        //Day 4 Hum
-        elHum4=$("#hum4");
-        dataHum4=response.list[3].main.humidity;
-        elHum4.append(dataHum4);             
+            //Day 4 Temp 
+            elTemp4=$("#temp4");
+            dataTemp4=response.list[3].main.temp;
+            dayTemp4=Math.floor((dataTemp3-273.15)*1.8+32);
+            elTemp4.append(dayTemp4);
+                //Day 4 Hum
+                elHum4=$("#hum4");
+                dataHum4=response.list[3].main.humidity;
+                elHum4.append(dataHum4);
+                   //Day 4 Icon
+                   let elIcon4=$("#icon4");
+                   let dataIcon4=response.list[3].weather[0].icon;
+                   let icon4Img=iconUrl+dataIcon4+"@2x.png";
+                   elIcon4.attr("src", icon4Img);
+                            
 
         //Day5 
         elDay5=$("#date5");
         dateDay5=moment().add(5, 'days').calendar();
         elDay5.append(dateDay5);
-
-        //Day 5 Temp 
-        elTemp5=$("#temp5");
-        dataTemp5=response.list[4].main.temp;
-        dayTemp5=Math.floor((dataTemp3-273.15)*1.8+32);
-        elTemp5.append(dayTemp5);
-
-        //Day 5 Hum
-        elHum5=$("#hum5");
-        dataHum5=response.list[4].main.humidity;
-        elHum5.append(dataHum5);        
+            //Day 5 Temp 
+            elTemp5=$("#temp5");
+            dataTemp5=response.list[4].main.temp;
+            dayTemp5=Math.floor((dataTemp3-273.15)*1.8+32);
+            elTemp5.append(dayTemp5);
+                //Day 5 Hum
+                elHum5=$("#hum5");
+                dataHum5=response.list[4].main.humidity;
+                elHum5.append(dataHum5);
+                   //Day 5 Icon
+                   let elIcon5=$("#icon5");
+                   let dataIcon5=response.list[4].weather[0].icon;
+                   let icon5Img=iconUrl+dataIcon5+"@2x.png";
+                   elIcon5.attr("src", icon5Img);
+                    
 });
 
 });
 
 });
 
-
-//oustanding task 
-
-/*
-- make a function of converting the tempature 
-- do a loop of 5 on the search button that displays the forcast 
-*/
